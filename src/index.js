9c44a07e06ed69651baeb2755fc5d2a50190e3f0
Brandon Shackelford <brandon.shackelford@docusign.com>
2021-11-01 18:43:45 -0500

    Add BShack theme

--- src/index.js
+++ src/index.js
@@ -46,20 +46,13 @@ const lightTheme = getClassicTheme({
 
 const darkTheme = getClassicTheme({
   style: "dark",
-  name: "GitHub Dark",
+  name: "BShack Dark",
 });
 
 // Write themes
 
 fs.mkdir("./themes", { recursive: true })
   .then(() => Promise.all([
-    fs.writeFile("./themes/light-default.json", JSON.stringify(lightDefaultTheme, null, 2)),
-    fs.writeFile("./themes/light-high-contrast.json", JSON.stringify(lightHighContrastTheme, null, 2)),
-    fs.writeFile("./themes/light-colorblind.json", JSON.stringify(lightColorblindTheme, null, 2)),
-    fs.writeFile("./themes/dark-default.json", JSON.stringify(darkDefaultTheme, null, 2)),
-    fs.writeFile("./themes/dark-high-contrast.json", JSON.stringify(darkHighContrastTheme, null, 2)),
-    fs.writeFile("./themes/dark-colorblind.json", JSON.stringify(darkColorblindTheme, null, 2)),
-    fs.writeFile("./themes/dark-dimmed.json", JSON.stringify(darkDimmedTheme, null, 2)),
     fs.writeFile("./themes/light.json", JSON.stringify(lightTheme, null, 2)),
     fs.writeFile("./themes/dark.json", JSON.stringify(darkTheme, null, 2)),
   ]))
