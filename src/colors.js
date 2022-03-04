6d8702079114aed14cff077bce12e0194bd71e14
Brandon Shackelford <brandonmshackelford@gmail.com>
2022-03-04 11:36:16 -0800

    Refactor colors

--- src/colors.js
+++ src/colors.js
@@ -1,28 +1,16 @@
-const lightColors = require("@primer/primitives/dist/json/colors/light.json");
-const darkColors = require("@primer/primitives/dist/json/colors/dark.json");
-const bshackColors = require("./colorsBShack.json")
+const lightColors = require("./colors/light.json");
+const darkColors = require("./colors/dark.json");
 
 function getColors(theme) {
 
   switch(theme) {
     case "light":
       return lightColors;
-    case "light_high_contrast":
-      return lightHighContrastColors;
-    case "light_colorblind":
-        return lightColorblindColors;
     case "dark":
       return darkColors;
-    case "dark_high_contrast":
-      return darkHighContrastColors;
-    case "dark_colorblind":
-      return darkColorblindColors;
-    case "dark_dimmed":
-      return dimmedColors;
     default:
       throw new Error(`Colors are missing for value: ${theme}`);
   }
-  return bshackColors;
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -7,41 +7,16 @@ const lightDefaultTheme = getTheme({
   name: "GitHub Light Default",
 });
 
-const lightHighContrastTheme = getTheme({
-  theme: "light_high_contrast",
-  name: "GitHub Light High Contrast"
-})
-
-const lightColorblindTheme = getTheme({
-  theme: "light_colorblind",
-  name: "GitHub Light Colorblind",
-});
-
 const darkDefaultTheme = getTheme({
   theme: "dark",
   name: "GitHub Dark Default",
 });
 
-const darkHighContrastTheme = getTheme({
-  theme: "dark_high_contrast",
-  name: "GitHub Dark High Contrast"
-})
-
-const darkColorblindTheme = getTheme({
-  theme: "dark_colorblind",
-  name: "GitHub Dark Colorblind"
-})
-
-const darkDimmedTheme = getTheme({
-  theme: "dark_dimmed",
-  name: "GitHub Dark Dimmed"
-})
-
 // Classic
 
 const lightTheme = getClassicTheme({
   style: "light",
-  name: "GitHub Light",
+  name: "BShack Light",
 });
 
 const darkTheme = getClassicTheme({
