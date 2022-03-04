ed77d31e5f22aab5333a4ede244caab0b3256e1a
Brandon Shackelford <brandon.shackelford@docusign.com>
2022-03-04 10:52:10 -0800

    Refactor colors

--- src/colors.js
+++ src/colors.js
@@ -1,10 +1,6 @@
 const lightColors = require("@primer/primitives/dist/json/colors/light.json");
-const lightHighContrastColors = require("@primer/primitives/dist/json/colors/light_high_contrast.json");
-const lightColorblindColors = require("@primer/primitives/dist/json/colors/light_colorblind.json");
 const darkColors = require("@primer/primitives/dist/json/colors/dark.json");
-const darkHighContrastColors = require("@primer/primitives/dist/json/colors/dark_high_contrast.json");
-const darkColorblindColors = require("@primer/primitives/dist/json/colors/dark_colorblind.json");
-const dimmedColors = require("@primer/primitives/dist/json/colors/dark_dimmed.json");
+const bshackColors = require("./colorsBShack.json")
 
 function getColors(theme) {
 
@@ -26,6 +22,7 @@ function getColors(theme) {
     default:
       throw new Error(`Colors are missing for value: ${theme}`);
   }
+  return bshackColors;
 }
 
 module.exports = {
